5e7b00c27477ced232872b0ea5ff1c380a67f0f8
Eleanor Lee <ellie007@gmail.com>
2014-07-01 12:02:04 -0500

    working implementation but not optimized, brute force approach

---
+++ lib/triangular_number.rb
@@ -0,0 +1,32 @@
+class HighlyDivisibleTriangularNumber
+
+  def generates_triangle_number(ordinal_tri_num)
+    (0..ordinal_tri_num).reduce(:+)
+  end
+
+  def find_all_factors(dividend_num)
+    num_of_factors = 0
+    (1..(dividend_num / 2)).each do |divisor_num|
+      if dividend_num % divisor_num == 0
+        num_of_factors += 1
+      end
+    end
+    num_of_factors += 1
+    num_of_factors
+  end
+
+  def highly_divisible_triangular_number(num_of_factors)
+    i = 0
+    until find_all_factors(generates_triangle_number(i)) > num_of_factors
+      #puts "this is #{i} : "
+      #puts "this is the triangle number #{generates_triangle_number(i)} : "
+      #puts "this is the factors #{find_all_factors(generates_triangle_number(i))}\n"
+      find_all_factors(generates_triangle_number(i))
+      i += 1
+    end
+    generates_triangle_number(i)
+  end
+
+end
+
+
---
+++ spec/triangular_number_spec.rb
@@ -0,0 +1,19 @@
+require 'triangular_number'
+
+describe HighlyDivisibleTriangularNumber do
+
+  let(:tri_num) { HighlyDivisibleTriangularNumber.new }
+
+  it 'generates a triangular number with a given value' do
+    expect(tri_num.generates_triangle_number(10)).to eq(55)
+  end
+
+  it 'does the prime factorization of a single number' do
+    expect(tri_num.find_all_factors(28)).to eq(6)
+  end
+
+  it 'find the highly divisible triangular number' do
+    expect(tri_num.highly_divisible_triangular_number(5)).to eq(28)
+  end
+
+end
